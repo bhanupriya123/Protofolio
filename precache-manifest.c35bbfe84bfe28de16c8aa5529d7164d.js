d4fd6b06b18e34562751c63140aae2258449e151
bhanupriya123 <bhanupriyarout5678@gmail.com>
2021-05-13 21:31:48 +0530

    Updates

---
+++ precache-manifest.c35bbfe84bfe28de16c8aa5529d7164d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b48f7acdff1ee9cdea8216a2ddf3d4b",
+    "url": "/Protofolio/index.html"
+  },
+  {
+    "revision": "3071edc59e7162b4d180",
+    "url": "/Protofolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a9af2157d45808dd98d3",
+    "url": "/Protofolio/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "3071edc59e7162b4d180",
+    "url": "/Protofolio/static/js/2.79be1882.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Protofolio/static/js/2.79be1882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9af2157d45808dd98d3",
+    "url": "/Protofolio/static/js/main.8b6969d8.chunk.js"
+  },
+  {
+    "revision": "28f10de8fd5487e0c969",
+    "url": "/Protofolio/static/js/runtime-main.02faf820.js"
+  }
+]);
